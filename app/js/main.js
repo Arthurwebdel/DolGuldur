3d1262b70e2d04e113b41e2925e006b5f30442c7
Artur <arturklochkoanenter@gmail.com>
2023-05-31 19:31:53 +0200

    task4

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,28 @@
 $(function () {
+    $('.btn').on('click', function () {
+        $('body').addClass('modal__open');
+       });
+
+    $('.modal__btn-close').on('click', function () {
+        $('body').removeClass('modal__open');
+       });   
+
     $(".menu [href]").each(function () {
         if (this.href == window.location.href) {
             $(this).addClass("current");
         }
     });
+
+
     $('.hero__background-box').slick({
         fade:true,
         autoplay: true,
+        arrows:false,
         autoplaySpeed: 3000
     });
     var mixer = mixitup('.works__gallery');
    $('.works__gallery').mixitup({
 
    });
+   
 });
--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,8 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
-    'app/js/main.js'
+    'app/js/main.js',
+    'app/js/modal.js'
   ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
