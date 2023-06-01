ee3e33f6f29122f05e79be8992f8959f92485637
Artur <arturklochkoanenter@gmail.com>
2023-06-01 15:03:49 +0200

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -20,8 +20,8 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
-    'app/js/modal.js',
-    'app/js/main.js'
+    'app/js/main.js',
+    'app/js/modal.js'
     
   ])
     .pipe(concat('main.min.js'))
